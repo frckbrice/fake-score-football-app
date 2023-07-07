11ccac5c1653c2912809eb1a21bcb575171eefdb
frckbrice <maebrie2017@gmail.com>
2023-07-07 15:40:23 +0100

    add pixel to overlay

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer text-white  h-20vh flex items-center justify-center text-center bg-indigo-950">
-      <p className="text-3xl ">Football App Reactjs Test 2023 &copy;</p>
+    <div className="footer text-white  h-20vh flex items-center justify-center text-center bg-indigo-950 ">
+      <p className="text-3xl font-Philosopher ">Football App Reactjs Test 2023 &copy;</p>
     </div>
   );
 };
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="header text-white  h-20vh flex items-center justify-center text-center bg-indigo-950">
-      <h1 className="text-8xl italic">Fake Football App ⚽</h1>
+    <header className="header text-white  h-20vh flex items-center justify-center text-center bg-indigo-950 ">
+      <h1 className="text-8xl italic font-Philosopher">Fake Football App ⚽</h1>
       <div>
         <img src="" alt="" />
       </div>
